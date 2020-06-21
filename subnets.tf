47a88d730662b49c9e478de59cd16bc130cf3893
kde6260 <kde6260@gmail.com>
2020-06-21 21:53:05 +0900

    #1 provider와 subnet 파일 분리

--- subnets.tf
+++ subnets.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  profile = "bookk-bookk"
-  region  = "ap-northeast-2"
-}
-
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = "true"
@@ -12,11 +7,11 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   tags = {
-    Name = "bookk_public_subnet_1"
+    Name = "bookk_public_subnet"
   }
 }
 
@@ -24,6 +19,6 @@ resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   tags = {
-    Name = "bookk_private_subnet_1"
+    Name = "bookk_private_subnet"
   }
 }
