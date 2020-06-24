7b3d99f3b4a198b799cf6d6c542bd35bfe03a936
kde6260 <kde6260@gmail.com>
2020-06-24 23:04:31 +0900

    #2 키페어 추가

--- dev/instances.tf
+++ dev/instances.tf
@@ -28,7 +28,16 @@ resource "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_key_pair" "access_key" {
+  key_name   = "dev-init-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbV8mfq1GOXQTkSLMrLTXjjJySyseJbkegqgahSLB2q3h/FVENy+no0pmtIeD1MdfsYxnjaIJs+l9gCcZWj4o9AXAOidcjkINeiRhS6Inayc9fbYmrH58d74KcHgFci+INbYjO+R4ybEVL/ScODIvEzagBdwzAgNr24vdxtSiP7jYFcOVsPJH9VtmgiSwgjMF1bfn/M9JOR84zgF73CNgzhNAqoCjTwqrQknmZZfddEDovnIj570wVSmTFn3PO58yQhlC3BPsFU/HD3bknVYiCrq25JD9u5VJfVr279Okpdvsv0weXxynnrzHwCL23D99JBu/kJr/yx3n50yQnOOKN c201809035@C201809035-01.local"
+  tags = {
+    Name = var.tag_name
+  }
+}
+
 resource "aws_instance" "dev" {
+  key_name          = aws_key_pair.access_key.key_name
   availability_zone = "ap-northeast-2a"
   ami               = aws_ami.ubuntu.id
   subnet_id         = aws_subnet.public_subnet.id
