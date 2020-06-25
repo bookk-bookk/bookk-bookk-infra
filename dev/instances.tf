9fb8153f1bdf477fc2feebffbb876d22dc235d2d
kde6260 <kde6260@gmail.com>
2020-06-25 12:19:51 +0900

    #2 AMI 직접 생성하지 않고 데이터소스로 받아옴

--- dev/instances.tf
+++ dev/instances.tf
@@ -1,30 +1,9 @@
-resource "aws_ebs_volume" "ebs" {
-  availability_zone = "ap-northeast-2a"
-  size              = 16
-  tags = {
-    Name = var.tag_name
-  }
-}
-
-resource "aws_ebs_snapshot" "ebs_snapshot" {
-  volume_id = aws_ebs_volume.ebs.id
-  tags = {
-    Name = var.tag_name
-  }
-}
-
-resource "aws_ami" "ubuntu" {
-  name                = "ami-024cd8aafb84bb8bc"
-  virtualization_type = "hvm"
-  root_device_name    = "/dev/xvda"
-  ena_support         = true
-  ebs_block_device {
-    device_name = "/dev/xvda"
-    snapshot_id = aws_ebs_snapshot.ebs_snapshot.id
-    volume_size = 16
-  }
-  tags = {
-    Name = var.tag_name
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "image-id"
+    values = ["ami-0d777f54156eae7d9"]
   }
 }
 
@@ -39,7 +18,7 @@ resource "aws_key_pair" "access_key" {
 resource "aws_instance" "dev" {
   key_name          = aws_key_pair.access_key.key_name
   availability_zone = "ap-northeast-2a"
-  ami               = aws_ami.ubuntu.id
+  ami               = data.aws_ami.ubuntu.id
   subnet_id         = aws_subnet.public_subnet.id
   instance_type     = "t3a.medium"
   user_data         = file("../install_docker.sh")
